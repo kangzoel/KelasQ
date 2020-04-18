a82d62f8d92dd05eaba6866960f7e0fcebc6e2d8
Samsul Arif Zulvian <saarizu@gmail.com>
2020-04-18 20:33:12 +0700

    buat view

---
+++ resources/views/klasses-edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Kelas')
+
+@section('content')
+    <form action="{{ route('klass.update', ['id' => $klass->id]) }}" method="POST" class="py-4 px-4">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Nama Kelas</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name') ?: $klass->name }}" autofocus>
+            @error('name')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="default_member_role_id">Peran Calon Anggota</label>
+            <select class="form-control @error('default_member_role_id') is-invalid @enderror" name="default_member_role_id" id="default_member_role_id">
+                @foreach (App\Role::orderBy('id', 'desc')->get() as $role)
+                    <option value="{{ $role->id }}" {{ $role->id == old('default_member_role_id') || $role->id == $klass->default_member_role_id && !old('default_member_role_id') ? 'selected' : '' }}>
+                        @switch($role->name)
+                            @case('admin')
+                                Ketua
+                                @break
+                            @case('secretary')
+                                Sekretaris
+                                @break
+                            @case('treasurer')
+                                Bendahara
+                                @break
+                            @default
+                                Anggota Biasa
+                        @endswitch
+                    </option>
+                @endforeach
+            </select>
+            @error('default_member_role_id')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="description">Deskripsi Kelas</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3">{{ old('description') ?: $klass->description }}</textarea>
+            @error('description')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
+        </div>
+        <div class="text-right mt-4">
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </div>
+    </form>
+@endsection
