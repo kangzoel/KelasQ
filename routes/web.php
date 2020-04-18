0158dce4e75fdc6becc4c49058f9629f5a96caa5
Samsul Arif Zulvian <saarizu@gmail.com>
2020-04-18 20:33:43 +0700

    tambah crud routes

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/classes', 'KlassController@index')->name('klass');
     Route::get('/classes/create', 'KlassController@create')->name('klass.create');
+    Route::get('/classes/{code}/edit', 'KlassController@edit')->name('klass.edit'); // TODO
     Route::get('/classes/{code}', 'KlassController@show')->name('klass.show');
     Route::get('/classes/{code}/tasks', 'KlassController@tasks')->name('klass.tasks');
     Route::get('/classes/{code}/schedules', 'KlassController@schedules')->name('klass.schedules');
@@ -28,8 +29,11 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::post('/classes/store', 'KlassController@store')->name('klass.store');
     Route::post('/classes/join', 'KlassController@join')->name('klass.join');
+    Route::post('/classes/out', 'KlassController@out')->name('klass.out'); // TODO
     Route::put('/classes/{code}/change_role', 'KlassController@change_role')->name('klass.change_role');
     Route::put('/classes/{code}/set_default_role', 'KlassController@set_default_role')->name('klass.set_default_role');
+    Route::put('/classes/{id}/update', 'KlassController@update')->name('klass.update'); // TODO
+    Route::delete('/classes/delete', 'KlassController@delete')->name('klass.delete'); // TODO
     Route::delete('/classes/{code}/kick', 'KlassController@kick')->name('klass.kick');
     Route::delete('/classes/{code}/ban', 'KlassController@ban')->name('klass.ban');
 });
