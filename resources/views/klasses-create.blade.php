2a227d1787fbb13c6d2de5421a3188cee839f2cf
Samsul Arif Zulvian <saarizu@gmail.com>
2020-04-18 20:23:32 +0700

    isi nilai lama descripsi

--- resources/views/klasses-create.blade.php
+++ resources/views/klasses-create.blade.php
@@ -39,7 +39,7 @@
         </div>
         <div class="form-group">
             <label for="description">Deskripsi Kelas</label>
-            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3"></textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3">{{ old('description') }}</textarea>
             @error('description')
                 <small class="text-danger">{{ $message }}</small>
             @enderror
@@ -48,49 +48,4 @@
             <button type="submit" class="btn btn-primary">Simpan</button>
         </div>
     </form>
-
-    <!-- Modal -->
-    <div class="modal fade" id="klassModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="h5 font-weight-bold modal-title" id="exampleModalScrollableTitle">Tambah Kelas</h2>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('klass.join') }}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <input type="text" class="form-control @error('code') is-invalid @enderror" name="code" placeholder="Kode Kelas" value="{{ old('code') }}">
-                            @error('code')
-                                <small class="text-danger">{{ $message }}</small>
-                            @enderror
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block font-weight-bold">
-                            <i class="fas fa-sign-in-alt mr-1"></i>
-                            Gabung Kelas
-                        </button>
-                    </form>
-                    <div class="text-center my-3">atau</div>
-                    <a href="/classes/create" class="btn btn-outline-primary btn-block font-weight-bold">
-                        <i class="fas fa-plus mr-1"></i>
-                        Buat Kelas
-                    </a>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batalkan</button>
-                </div>
-            </div>
-        </div>
-    </div>
 @endsection
-
-@push('scripts')
-    <script>
-        $('#klassModal').on('shown.bs.modal', function() {
-            $('[name=code]').select()
-        })
-    </script>
-@endpush
