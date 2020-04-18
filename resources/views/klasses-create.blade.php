6300d0134fe5b806bc8326eaaeb8b7ebcb9ce7b9
Samsul Arif Zulvian <saarizu@gmail.com>
2020-04-18 20:16:46 +0700

    ubah validasi

--- resources/views/klasses-create.blade.php
+++ resources/views/klasses-create.blade.php
@@ -5,15 +5,17 @@
 @section('content')
     <form action="{{ route('klass.store') }}" method="POST" class="py-4 px-4">
         @csrf
-        <x-alert></x-alert>
         <div class="form-group">
             <label for="name">Nama Kelas</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name') }}">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{ old('name') }}" autofocus>
+            @error('name')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="form-group">
-          <label for="default_member_role_id">Peran Calon Anggota</label>
-          <select class="form-control @error('default_member_role_id') is-invalid @enderror" name="default_member_role_id" id="default_member_role_id">
-                @foreach (App\Role::all() as $role)
+            <label for="default_member_role_id">Peran Calon Anggota</label>
+            <select class="form-control @error('default_member_role_id') is-invalid @enderror" name="default_member_role_id" id="default_member_role_id">
+                @foreach (App\Role::orderBy('id', 'desc')->get() as $role)
                     <option value="{{ $role->id }}" {{ $role->id == old('default_member_role_id') ? 'selected' : '' }}>
                         @switch($role->name)
                             @case('admin')
@@ -30,11 +32,17 @@
                         @endswitch
                     </option>
                 @endforeach
-          </select>
+            </select>
+            @error('default_member_role_id')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="form-group">
             <label for="description">Deskripsi Kelas</label>
             <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="3"></textarea>
+            @error('description')
+                <small class="text-danger">{{ $message }}</small>
+            @enderror
         </div>
         <div class="text-right mt-4">
             <button type="submit" class="btn btn-primary">Simpan</button>
