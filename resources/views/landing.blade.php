0028e65403b4367cafa5bacdf54e7bcda1de7e92
Samsul Arif Zulvian <saarizu@gmail.com>
2020-04-20 03:19:43 +0700

    ubah layout

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,10 +8,9 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <style>
         body {
-            padding: 0
-        }
-        #app {
-            height: 100vh
+            padding: 0;
+            height: 100vh;
+            padding-bottom: 12px;
         }
         p {
             line-height: 1.60em!important
@@ -21,16 +20,14 @@
         }
     </style>
 </head>
-<body>
-    <div id="app" class="d-flex flex-column align-items-center justify-content-center">
-        <img src="{{ asset('img/Logo.png') }}" alt="Logo KelasQ">
-        <h1 class="font-weight-bold mb-1">{{ config('app.name') }}</h1>
-        <p class="h5 px-5 text-center mb-3">Aplikasi manajemen kelas mahasiswa</p>
-        <nav>
-            <a href="/login" class="btn btn-dark mr-2">Masuk</a>
-            <a href="/register" class="btn btn-outline-dark">Daftar</a>
-        </nav>
-    </div>
+<body class="d-flex flex-column align-items-center justify-content-center">
+    <img src="{{ asset('img/Logo.png') }}" alt="Logo KelasQ">
+    <h1 class="font-weight-bold mb-1">{{ config('app.name') }}</h1>
+    <p class="h5 px-5 text-center mb-3">Aplikasi manajemen kelas mahasiswa</p>
+    <nav>
+        <a href="/login" class="btn btn-dark mr-2">Masuk</a>
+        <a href="/register" class="btn btn-outline-dark">Daftar</a>
+    </nav>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
